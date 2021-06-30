4f2923d9d03700c82c72a9e47ab460ba25757e48
Jakezo <cakim21@naver.com>
2021-06-30 18:42:50 +0900

    23e21

--- src/main/java/com/koreait/study/HomeController.java
+++ src/main/java/com/koreait/study/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locaddleddddsad{}.", loasasca);
+		logger.info("Welcome home! The client locaddledddddddsad{}.", loasasca);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
